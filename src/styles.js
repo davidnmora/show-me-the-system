9791ea60ab9110b55d60b4e51e83c0a8e80c1ff1
davidnmora <davidnmora@gmail.com>
2020-05-10 22:24:50 -0700

    Do basic app setup: packages, delete boilerplate, prettier

---
+++ src/styles.js
@@ -0,0 +1,29 @@
+// import { animated } from 'react-spring'
+import styled, { createGlobalStyle } from 'styled-components'
+
+const COLORS = {
+  dark: '#2F2F2F',
+  light: '#eee',
+  cool: '#9feeeb',
+  hot: '#eea8e9',
+}
+
+export const GlobalStyle = createGlobalStyle`
+  body {
+    padding: 48px;
+    font-family: 'PT Sans', sans-serif;
+    background-color: ${COLORS.dark};
+    color: ${COLORS.light};
+  }
+`
+
+export const Headline = styled.div`
+  font-size: 64px;
+  font-weight: bolder;
+  line-height: 40px;
+`
+
+export const Subtitle = styled.div`
+  font-size: 24px;
+  font-weight: lighter;
+`
